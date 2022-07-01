64f0279bc08066d65636f9222a11d508c2579768
Josue Simba <84150141+Jostch@users.noreply.github.com>
2022-07-01 13:49:17 +0200

    New file

---
+++ js/mian.js
@@ -0,0 +1,42 @@
+// Créons un objet en javascripts en ligne d'instruction
+
+/*
+var nomObjet = new Object();
+objet.propriete1 = valeur1;
+objet.propriete2 = valeur2;
+objet.methode1 = function name(parametre1,parametre2) {
+    instruction1;
+    instruction2;
+    etc;
+};
+
+// Créons un objet en javascritp en bloc
+
+var nomObjet = {
+    propriete1 : valeur1,
+    propriete2 : valeur2,
+
+    methode1 : function(parametre1,parametre2,etc) {
+        instruction1;
+        instruction2;
+        etc;
+    }
+};
+
+*/
+
+var voiture = {
+    coleur : "Verte",
+    nombrePorte : 4,
+    nombreRou : 5,
+    boiteVitesse : "Manuelle",
+    typeMoteur : "Essence",
+    
+    demarer : function() {
+        document.write("La voiture démare");
+    },
+    
+    eclairage : function() {
+        document.write("La voiture eclaire la route pendant qu'il fait noir !");
+    }
+}
